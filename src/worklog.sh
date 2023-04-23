e2f650f1207c454b478668d30d9363ca576fcf5f
Rômulo Costa <romulopinheirocosta@gmail.com>
2023-04-23 01:23:09 -0300

    Fix case sensitive issues with arguments and more
    Refactor code base
    add get_last_log_event() function
    add is_recent() function
    replace a bunch of conditionals in check_status() function with switch case

--- src/worklog.sh
+++ src/worklog.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/bash
-
+#!/usr/bin/bash 
 CONFIG_DIR=/home/romulo/Repos/worklog/conf/worklog.conf
 
 usage(){
+	# Show how to use the program
 	echo "$0 usage:" 
 	echo "$0 -p PROJECT_NAME -s STATUS [-d LOG_DIR] event_message"
 	echo -e "\tSTATUS: start, pause, resume, end"
@@ -10,13 +10,30 @@ usage(){
 }
 
 create_log_file(){
+	# create the log file inside the choosen directory
 	mkdir -pv "${LOG_DIR%\/*}"
 	touch "$LOG_DIR"
 }
 
-check_status(){
-	# check if given project status is already has been proccessed
+get_last_log_event(){
+	# get last status event and timestamp of the given project
+	grep "$PROJECT" "$LOG_DIR" | \
+		grep "$1" | \
+		tail -1 | \
+		cut -d " " -f 1-2
+}
 
+is_recent(){
+	# Check if the first argument is most recent than the second argument
+	if [[ "$1" -lt "$2" ]]; then
+		echo 1;
+	else
+		echo 0;
+	fi
+}
+
+check_status(){
+	# check if given project status has already been proccessed
 	if [ ! -f "$LOG_DIR" ]; then
 		# if file does not exists, then
 		create_log_file
@@ -24,66 +41,58 @@ check_status(){
 
 	last_record=$(grep "$PROJECT" "$LOG_DIR")
 	
-	# get last START event and timestamp of the given project
-	last_start=$(grep "$PROJECT" "$LOG_DIR" | grep "START" | tail -1 | cut -d " " -f 1-2)
+	last_start=$(get_last_log_event "START")
 	last_start=$(date -d "$last_start" +%s)
 
-	# get last END event and timestamp of the given project
-	last_end=$(grep "$PROJECT" "$LOG_DIR" | grep "END" | tail -1 | cut -d " " -f 1-2) 
+	last_end=$(get_last_log_event "END")
 	last_end=$(date -d "$last_end" +%s)
 
-	# get last PAUSE event and timestamp of the given project
-	last_pause=$(grep "$PROJECT" "$LOG_DIR" | grep "PAUSE" | tail -1 | cut -d " " -f 1-2)
+	last_pause=$(get_last_log_event "PAUSE")
 	last_pause=$(date -d "$last_pause" +%s)
 
-	# get last RESUME event and timestamp of the given project
-	last_resume=$(grep "$PROJECT" "$LOG_DIR" | grep "RESUME" | tail -1 | cut -d " " -f 1-2)
+	last_resume=$(get_last_log_event "RESUME")
 	last_resume=$(date -d "$last_resume" +%s)
-	now=$(date +%s)
 	
 	if [ ! -z "$last_record" ];then
 		# check if each status occurred before it's counter part
+		has_started_after_a_end=$(is_recent "$last_end" "$last_start")
+		has_ended_after_a_start=$(is_recent "$last_start" "$last_end")
+		has_paused_after_a_resume=$(is_recent "$last_resume" "$last_pause")
 
-		has_started_after_a_pause=$([ "$last_pause" -lt "$last_start" ] && echo 1 || echo 0)
-		has_started_after_a_end=$([ "$last_end" -lt "$last_start" ] && echo 1 || echo 0)
-		has_ended_after_a_start=$([ "$last_start" -lt "$last_end" ] && echo 1 || echo 0)
-		has_paused_after_a_resume=$([ "$last_resume" -lt "$last_pause" ] && echo 1 || echo 0)
+		case "$STATUS" in
+			START) # User chooses to start a project
+				if [ "$has_started_after_a_end" == 1 ]; then
+					echo "Project [$PROJECT] already is marked as [$STATUS]"
+					exit 1
+				fi
+				;;
+			END) # User chooses to end a project
+				if [ "$has_paused_after_a_resume" == 1 ]; then
+					echo "Project [$PROJECT] is PAUSED. RESUME before END"
+					exit 1
+				fi
 
-		if [ "$STATUS" == "START" ]; then
-			if [ "$has_started_after_a_end" == 1 ]; then
-				echo "The project [$PROJECT] already is marked as [$STATUS]"
-				exit 1
-			fi
-		fi
+				if [ "$has_ended_after_a_start" == "1" ]; then
+					echo "Project [$PROJECT] is marked as [$STATUS] already "
+					exit 1
+				fi
+				;;
+			PAUSE) # User chooses to Pause a project
+				if [ "$has_paused_after_a_resume" == "1" ]; then
+					echo "Project [$PROJECT] is marked as [$STATUS] already"
+					exit 1
+				fi
+				;;
+			RESUME) # User chooses to Resume a project
+				if [ "$has_paused_after_a_resume"  == "0"  ]; then
+					echo "Project [$PROJECT] is marked as [$STATUS] already "
+					exit 1
+				fi
+				;;
+			esac
 
-		if [ "$STATUS" == "END" ]; then
-			if [ "$has_paused_after_a_resume" == 1 ]; then
-				echo "The project [$PROJECT] is PAUSED please RESUME before END"
-				exit 1
-			fi
-
-			if [ "$has_ended_after_a_start" == "1" ]; then
-				echo "The project [$PROJECT] already is marked as [$STATUS]"
-				exit 1
-			fi
-		fi
-
-		if [ "$STATUS" == "PAUSE" ]; then
-			if [ "$has_paused_after_a_resume" == "1" ]; then
-				echo "This project [$PROJECT] already is marked as [$STATUS]"
-				exit 1
-			fi
-		fi
-
-		if [ "$STATUS" == "RESUME" ]; then
-			if [ "$has_paused_after_a_resume"  == "0"  ]; then
-				echo "This project Status for [$PROJECT] already is marked as [$STATUS]"
-				exit 1
-			fi
-		fi
-		
 	elif [ "$STATUS" != "START" ]; then
-		echo "there is no way to mark status [$STATUS] because the log is empty"
+		echo "Can't mark status [$STATUS] because the log is empty"
 		exit 1
 	fi
 }
@@ -152,10 +161,10 @@ read_user_options(){
 				LOG_DIR="${OPTARG}"
 				;;
 			s)	# Specify status of the project [start,end,pause]
-				if [[ "$OPTARG" != "start" ]] &&
-					[[ "$OPTARG" != "end" ]] &&
-					[[ "$OPTARG" != "resume" ]] &&
-					[[ "$OPTARG" != "pause" ]]; then
+				if [[ "${OPTARG,,}" != "start" ]] &&
+					[[ "${OPTARG,,}" != "end" ]] &&
+					[[ "${OPTARG,,}" != "resume" ]] &&
+					[[ "${OPTARG,,}" != "pause" ]]; then
 					echo "invalid status passed as argument -s $OPTARG"
 					exit 1;
 				fi;
@@ -176,7 +185,7 @@ main(){
 
 	# remove user options from final event message
 	# necessary due the scope of getopts in 'read_user_options' function
-	shift "$(($OPTIND-1))"				
+	shift "$((OPTIND-1))"				
 
 	EVENT_MESSAGE="$*"							
 
